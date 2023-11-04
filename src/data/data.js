776a10e1a49ff3c6d90e8ee3aee23acbdabc2e07
vishnu7287 <vishnu.kmk7287@gmail.com>
2023-11-05 00:54:18 +0530

    Initial commit

---
+++ src/data/data.js
@@ -0,0 +1,36 @@
+export const data = [
+  {
+    id: 1,
+    img: "https://i.postimg.cc/VkRWdHWx/57214s81.jpg",
+    title: "Multimedia Web",
+    desc: "",
+    link: "",
+    git: "",
+  },
+  {
+    id: 2,
+    img: "https://i.postimg.cc/Bv75CLyN/4167022.jpg",
+    title: "Tarvel Agency",
+    link: "",
+    git: "",
+  },
+  {
+    id: 3,
+    img: "https://i.postimg.cc/wBjcJcW7/5890625.jpg",
+    title: "Music Player",
+    desc: "",
+    link: "",
+    git: "",
+  },
+];
+
+export const personalData = {
+  fullName: "Pathakamuri Siddartha",
+  linkedInUrl: "https://www.linkedin.com/in/pathakamuri-siddartha-671a9415a/",
+  githubUrl: "https://github.com/Siddartha03",
+  instagramUrl:
+    "https://instagram.com/siddartha_pathakamuri?igshid=MmVlMjlkMTBhMg==",
+  fbUrl: "https://www.facebook.com/pathakamuri.siddartha.7?mibextid=gjUFcU",
+  teligramUrl: "https://t.me/siddartha2000",
+  whatsAppUrl: "https://wa.me/7287992262",
+};
